734a408ffcd29d7c8f19221772c2cb608d7d5f67
Zeeshan Khan <zkhan1093@gmail.com>
2016-07-10 05:01:21 +0530

    some fixes

--- projects/settings.py
+++ projects/settings.py
@@ -77,6 +77,13 @@ STATICFILES_DIRS = (
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+# Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
